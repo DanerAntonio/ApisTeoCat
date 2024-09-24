800058f1d9d7ee88257cddb299858b4d5f3cb4f7
CristoteAma <danerbedoya@gmail.com>
2024-09-24 16:30:14 -0500

    Arreglar detalleVentasController y subir cambios

--- src/routes/detalleVentasRoutes.js
+++ src/routes/detalleVentasRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const detalleVentaController = require('../controllers/detalleVentasController');
+const detalleVentaController = require('../controllers/detalleventasController');
 
 router.post('/', detalleVentaController.createDetalleVenta);
 router.get('/', detalleVentaController.getDetalleVentas);
